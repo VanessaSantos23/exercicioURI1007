65450f97dbe3bbae27c6c0fd651478e4ba1f3919
Vanessa Santos <nbsantos34@gmail.com>
2022-02-01 23:28:26 -0300

    exercicio resolvido uri

---
+++ URI1007/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI1007
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C, D, dif;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            C = int.Parse(Console.ReadLine());
+            D = int.Parse(Console.ReadLine());
+
+            dif = A * B - C * D;
+
+            Console.WriteLine("DIFERENÇA = " + dif);
+
+            Console.ReadLine();
+        }
+    }
+}
